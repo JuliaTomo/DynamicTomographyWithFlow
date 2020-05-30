0210247c25c2ff0e231ce56dd110f7ba888ffb0a
jakeoung <jakeoung@gmail.com>
2020-05-30 23:29:00 +0200

    change TNV -> CTV and add (linf,1,1) in ctv

--- src/XfromProjections.jl
+++ src/XfromProjections.jl
@@ -12,7 +12,7 @@ export filter_proj, bp_slices
 # iterative
 include("iterative/util_convexopt.jl")
 include("iterative/tv_primaldual.jl")
-include("iterative/tnv_primaldual.jl")
+include("iterative/cnv_primaldual.jl")
 include("iterative/sirt.jl")
 export recon2d_sirt!, recon2d_slices_sirt!, _compute_sum_rows_cols
 export recon2d_tv_primaldual!, recon2d_slices_tv_primaldual!, recon2d_ctv_primaldual!
