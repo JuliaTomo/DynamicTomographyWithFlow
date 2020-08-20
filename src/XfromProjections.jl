a256d51546b000abe8a4d5cf39a417d56a2f4ecb
jakeoung <jakeoung@gmail.com>
2020-08-20 14:46:23 +0200

    refactor slices -> stack

--- src/XfromProjections.jl
+++ src/XfromProjections.jl
@@ -14,7 +14,7 @@ include("iterative/util_convexopt.jl")
 include("iterative/tv_primaldual.jl")
 include("iterative/ctv_primaldual.jl")
 include("iterative/sirt.jl")
-export recon2d_sirt!, recon2d_slices_sirt!, _compute_sum_rows_cols
-export recon2d_tv_primaldual!, recon2d_slices_tv_primaldual!, recon2d_ctv_primaldual!
+export recon2d_sirt!, recon2d_stack_sirt!, _compute_sum_rows_cols
+export recon2d_tv_primaldual!, recon2d_stack_tv_primaldual!, recon2d_ctv_primaldual!
 
 end
