dfe78425806922551c2326f7792db682d057db0f
Koo <jakeoung@gmail.com>
2021-08-15 01:15:16 +0100

    minor

--- examples/fbp_slices.jl
+++ examples/fbp_slices.jl
@@ -12,7 +12,10 @@ nangles = 90
 detcount = Int(floor(size(img,1)*1.4))
 proj_geom = ProjGeom(1.0, detcount, LinRange(0,pi,nangles+1)[1:nangles])
 
-A = fp_op_parallel2d_strip(proj_geom, size(img, 1), size(img, 2))
+isdefined_A = @isdefined A
+if isdefined_A == false
+    A = fp_op_parallel2d_strip(proj_geom, size(img, 1), size(img, 2))
+end
 
 p = zeros(nangles, nslice, detcount)
 for i=1:nslice
@@ -23,6 +26,5 @@ end
 q = filter_proj(p)
 img3d = bp_slices(q, A, H, W)
 
-using PyPlot
-pygui(true)
-imshow(img3d[:,:,60])
+# using PyPlot
+# imshow(img3d[:,:,60])
--- examples/sirt2d_stack.jl
+++ examples/sirt2d_stack.jl
@@ -13,7 +13,10 @@ nangles = 90
 detcount = Int(floor(size(img,1)*1.4))
 proj_geom = ProjGeom(1.0, detcount, LinRange(0,pi,nangles+1)[1:nangles])
 
-A = fp_op_parallel2d_strip(proj_geom, size(img, 1), size(img, 2))
+isdefined_A = @isdefined A
+if isdefined_A == false
+    A = fp_op_parallel2d_strip(proj_geom, size(img, 1), size(img, 2))
+end
 
 p = zeros(nangles, nslice, detcount)
 for i=1:nslice
@@ -24,6 +27,5 @@ p = permutedims(p, [2,3,1])
 u = zeros(size(img))
 @time recon2d_stack_sirt!(u, A, p, 20);
 
-using PyPlot
-pygui(true)
-imshow(u[:,:,60])
+# using PyPlot
+# imshow(u[:,:,60])
